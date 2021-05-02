dd318733346d1c9e2e46717f3efc589a7e834dd0
phj2724 <74810236+phj2724@users.noreply.github.com>
2021-05-02 12:50:41 +0900

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -17,11 +17,11 @@ def get_start_time(ticker):
     start_time = df.index[0]
     return start_time
 
-def get_ma15(ticker):
-    """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
-    ma15 = df['close'].rolling(15).mean().iloc[-1]
-    return ma15
+def get_ma5(ticker):   
+    """5일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=5)
+    ma5 = df['close'].rolling(5).mean().iloc[-1]
+    return ma5
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -45,21 +45,21 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-XRP")
         end_time = start_time + datetime.timedelta(days=1)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
-            current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and ma15 < current_price:
+        if start_time < now < end_time - datetime.timedelta(seconds=32400): # 9:00 ~ 12:00(오전)
+            target_price = get_target_price("KRW-XRP", 0.005)
+            ma5 = get_ma5("KRW-XRP")
+            current_price = get_current_price("KRW-XRP")
+            if target_price < current_price and ma5 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
         else:
-            btc = get_balance("BTC")
-            if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+            btc = get_balance("XRP")
+            if btc > 2:
+                upbit.sell_market_order("KRW-XRP", btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
